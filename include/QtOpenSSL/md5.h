1b399e490a5cc8487d3ecf9add222cb159a7ed6e
sjc0910 <sjc.0910@qq.com>
2022-01-23 20:38:17 +0800

    MD4 algorithm is insecure. Use MD5 instead.

--- include/QtOpenSSL/md5.h
+++ include/QtOpenSSL/md5.h
@@ -2,7 +2,6 @@
 #include <QByteArray>
 namespace md5 {
     QByteArray md5(QByteArrayView source);
-    QByteArray md4(QByteArrayView source);
     QByteArray md5_16bit(QByteArrayView source);
-    constexpr qsizetype md5_size = 16, md4_size = 16;
+    constexpr qsizetype md5_size = 16;
 }
